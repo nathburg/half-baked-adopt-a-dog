7400831033c3f760a597b6dbc09e0bf0f7476f35
Nathan Burgess <nathans.email.address@gmail.com>
2022-08-08 16:59:46 -0700

    dog cards load

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import { getDogs } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
+import { getDog } from './fetch-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
 // on load
 // fetch all dogs
+
+
+loadData();
 // render and append all dog cards to the container
+
+async function loadData() {
+    const dogs = await getDogs();
+    for (let dog of dogs) {
+        const dogDiv = renderDogCard(dog);
+        dogListContainer.append(dogDiv);
+    }
+}
