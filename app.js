01096db9cb2bd8aa9afa8bb872660442bb4b11bf
Nathan Burgess <nathans.email.address@gmail.com>
2022-08-08 17:52:56 -0700

    dog detail pages have their information

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ const dogListContainer = document.getElementById('dog-list-container');
 // on load
 // fetch all dogs
 
-
 loadData();
 // render and append all dog cards to the container
 
 async function loadData() {
     const dogs = await getDogs();
     for (let dog of dogs) {
+        console.log(dog.name);
         const dogDiv = renderDogCard(dog);
         dogListContainer.append(dogDiv);
     }
 }
+
